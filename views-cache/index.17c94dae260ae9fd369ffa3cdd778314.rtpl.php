5d4774cab07a094dc000a8d534f2ab5b29c83d92
Fabiobarsil <fabiobarsil@hotmail.com>
2019-05-23 15:34:47 -0300

    Admin - Categorias

--- views-cache/index.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/index.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -1,1 +1,307 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Hello!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?>    <div class="slider-area">
+        	<!-- Slider -->
+			<div class="block-slider block-slider4">
+				<ul class="" id="bxslider-home4">
+					<li>
+						<img src="/res/site/img/h4-slider.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								Brummers<span class="primary">Lançamento<strong>2018</strong></span>
+							</h2>
+							<h4 class="caption subtitle">Coleção Gen Benício</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+						</div>
+					</li>
+					<li><img src="/res/site/img/h4-slider2.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								A Frente Oriental<span class="primary">Lançamento <strong>2018</strong></span>
+							</h2>
+							<h4 class="caption subtitle">Coleção Gen Benício</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+						</div>
+					</li>
+					<!--<li><img src="/res/site/img/h4-slider3.png" alt="Slide">
+						<div class="caption-group">
+							<h2 class="caption title">
+								Guerra da França no Mali<span class="primary">Lançamento <strong>2018</strong></span>
+							</h2>
+							<h4 class="caption subtitle">Coleção Gen Benício</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+						</div>
+					</li>
+					<li><img src="/res/site/img/h4-slider4.png" alt="Slide">
+						<div class="caption-group">
+						  <h2 class="caption title">
+								Marchando para o inferno<span class="primary">Lançamento <strong>2018</strong></span>
+							</h2>
+							<h4 class="caption subtitle">Coleção Gen Benício</h4>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+						</div>
+					</li>-->
+				</ul>
+			</div>
+			<!-- ./Slider -->
+    </div> <!-- End slider area -->
+    
+    <!--<div class="#promo-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo1">
+                        <i class="fa fa-refresh"></i>
+                        <p>Editoriais</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo2">
+                        <i class="fa fa-truck"></i>
+                        <p>Frete Grátis</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo3">
+                        <i class="fa fa-lock"></i>
+                        <p>Pagamento seguro</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo4">
+                        <i class="fa fa-gift"></i>
+                        <p>Revistas</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>--> <!-- End promo area -->
+    
+    <div class="maincontent-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Os mais vendidos</h2>
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum </a></h2>
+                                
+                                <div class="product-carousel-price">
+                                    <ins>$30.00</ins> <del>$70.00</del>
+                                </div> 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum </h2>
+                                <div class="product-carousel-price">
+                                    <ins>$89.00</ins> <del>$99.00</del>
+                                </div> 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum</h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$40.00</ins> <del>$45.00</del>
+                                </div>                                 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum</a></h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$20.00</ins> <del>$25.00</del>
+                                </div>                            
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum</h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$10.00</ins> <del>$15.00</del>
+                                </div>                                 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum</a></h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$40.00</ins>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End main content area -->
+    <div class="maincontent-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Os mais vistos da Semana</h2>
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum </a></h2>
+                                
+                                <div class="product-carousel-price">
+                                    <ins>$30.00</ins> <del>$70.00</del>
+                                </div> 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum </h2>
+                                <div class="product-carousel-price">
+                                    <ins>$89.00</ins> <del>$99.00</del>
+                                </div> 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum</h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$40.00</ins> <del>$45.00</del>
+                                </div>                                 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum</a></h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$20.00</ins> <del>$25.00</del>
+                                </div>                            
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2>Lorem ipsum</h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$10.00</ins> <del>$15.00</del>
+                                </div>                                 
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="/res/site/img/product.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                                
+                                <h2><a href="#">Lorem ipsum</a></h2>
+
+                                <div class="product-carousel-price">
+                                    <ins>$40.00</ins>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End main content area -->
+    
+    <div class="brands-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="brand-wrapper">
+                        <div class="brand-list">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">
+                            <img src="/res/site/img/brand10.png" alt="">                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End brands area -->
+ 
